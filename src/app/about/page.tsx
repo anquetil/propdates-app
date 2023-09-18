031135a20df7cf3a33060b9c1eeb8dc43897710a
Martin <25070051+anquetil@users.noreply.github.com>
2023-09-18 13:58:46 +0200

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,6 @@ import { IBM_Plex_Mono } from 'next/font/google'
 import { LoadingNoggles } from '@/components/LoadingNoggles'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { Address, useAccount, useNetwork, useSwitchNetwork } from 'wagmi'
-import useGetActions from '@/hooks/useGetActions'
-import { Actions } from '@/components/Actions'
-import Link from 'next/link'
 import { PageTitle } from '@/components/PageTitle'
 import ReactMarkdown from 'react-markdown'
 
@@ -17,13 +14,13 @@ export default function AboutPage() {
       '\n\nIts goal is to make it easier for proposers to keep the Nouns community updated on their work.'
    md +=
       '\n\nTo set it up, a user needs to claim the admin role or transfer it to another address via the [admin page](https://propdates.wtf/admin).'
-   md += '\n\nAll proposals updates are automatically refunded by the contract.'
+   md += '\n\nThe gas fees of all proposals updates are automatically refunded by the contract.'
    md +=
       '\n\nContract Address: [0x94b4fb16893c0fb4e470eef2559c24fd87fed5f1](https://etherscan.io/address/0x94b4fb16893c0fb4e470eef2559c24fd87fed5f1)'
 
    return (
       <div className=''>
-         <PageTitle title={'What is Propdates?'} home={true} />
+         <PageTitle title={'What is Propdates?'} />
 
          <ReactMarkdown
             linkTarget={'_blank'}
