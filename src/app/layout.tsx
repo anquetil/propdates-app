f74d215be0d7ffbda00d4e45b6fddcf24273b8c8
Martin <25070051+anquetil@users.noreply.github.com>
2023-09-22 13:58:37 +0200

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-   title: 'Propdates',
+   title: 'Nouns Updates',
    description: 'Onchain Proposal Updates',
 }
 
