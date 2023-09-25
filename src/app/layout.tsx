4b2ceb9cf3eff441ab64c760d1a412dcf734d7a9
Martin <25070051+anquetil@users.noreply.github.com>
2023-09-25 09:52:12 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
                      {children}
                   </main>
                </div>
+               <Analytics />
             </Providers>
          </body>
       </html>
