a5d591fc34116ecd14c26ecef092235690e1e5ce
Martin <25070051+anquetil@users.noreply.github.com>
2023-12-11 11:39:08 +0100

    add tooltip on hover when not connected

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -30,20 +30,30 @@ export function LikeButton({ updateId }: { updateId: string }) {
                   <HeartFilledIcon />
                </div>
             ) : (
-               <div
-                  onClick={() => {
-                     if(isConnected){
-                        setForceFill(true)
-                        likeMutation.mutate()
-                     }
-                  }}
-                  className={`text-gray-600 ${
-                     isConnected &&
-                     'hover:cursor-pointer active:text-red-600 active:scale-125 ease-in-out transition-all'
-                  }`}
-               >
-                  <HeartIcon />
+               <div className='relative group'>
+                 <div
+                     className={`z-100 text-xs bottom-5 -translate-x-[40%] whitespace-nowrap py-1 w-fit absolute 
+                     px-2 bg-black rounded-sm text-white ease-in-out transition-all duration-200 
+                     group-hover:bg-opacity-90 group-hover:text-opacity-90 bg-opacity-0 text-opacity-0`}
+                     >
+                        {`Connect your wallet to like!`}
+                     </div>
+                     <div
+                        onClick={() => {
+                           if (isConnected) {
+                              setForceFill(true)
+                              likeMutation.mutate()
+                           }
+                        }}
+                        className={`text-gray-600 ${isConnected &&
+                           'hover:cursor-pointer active:text-red-600 active:scale-125 ease-in-out transition-all'
+                           }`}
+                     >
+
+                        <HeartIcon />
+                     </div>
                </div>
+
             )}
          </div>
          <div className={userLiked ? 'text-red-600' : 'text-gray-600'}>
